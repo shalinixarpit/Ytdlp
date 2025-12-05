565eea6c8c7b553daed2dd00b9dd4c4165d69571
shalinixarpit <iskausekarnekeliye@gmail.com>
2025-12-05 11:32:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 import yt_dlp
 from pyrogram import Client, filters
 
-BOT_TOKEN = "7567559210:AAH8ZHbf_DCLr0dPe1gN8NvS-_EifLj7VIA"  # <-- Token yahan daalo
+BOT_TOKEN = "7834877953:AAFxCNHmyuoCT2y6IMm0zsxuGxQo8vsY0QM"  # <-- Token yahan daalo
 DOWNLOAD_PATH = "./downloads/"
 os.makedirs(DOWNLOAD_PATH, exist_ok=True)
 
